009be432a5b8a1af5e093cfa7695e97ea0144756
Isabella Pereira <isabellapereira251@gmail.com>
2025-11-21 16:07:15 -0600

    all linter errors but one

--- project3/webServer.js
+++ project3/webServer.js
@@ -13,13 +13,13 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { ObjectId } from "mongodb";
 import session from "express-session";
-import multer from "multer"
+import multer from "multer";
+import fs from 'node:fs';
 
 import User from "./schema/user.js";
 import Photo from "./schema/photo.js";
 import SchemaInfo from "./schema/schemaInfo.js";
-import { request } from "http";
-import fs from 'node:fs'
+
 
 const portno = 3001; // Port number to use
 const app = express();
@@ -64,7 +64,7 @@ app.use(express.static(__dirname));
 
 app.use(express.json());
 
-app.get("/", function (request, response) {
+app.get("/", function (_request, response) {
   response.send("Simple web server of files from " + __dirname);
 });
 
@@ -171,6 +171,7 @@ app.post('/user', async (request, response) => {
       };
       return response.status(201).json({id: savedUser._id, username: savedUser.login_name, first_name: savedUser.first_name});
     }
+    return response.status(500).send("Failed to create user");
    } catch (err){ 
     console.error(err);
     return response.status(500).send("Failed to create user");
@@ -404,8 +405,8 @@ app.get('/comments/:id', async (request, response) => {
 
 app.post('/commentsOfPhoto/:photoId', async (request, response) => {
 
-  console.log("Entering (post, /commentsOfPhoto/:photoId) endpoint")
-  let comment = request.body.comment
+  console.log("Entering (post, /commentsOfPhoto/:photoId) endpoint");
+  let comment = request.body.comment;
   let photoId = request.params.photoId;
   let userId = request.session.user.id;
 
@@ -418,31 +419,30 @@ app.post('/commentsOfPhoto/:photoId', async (request, response) => {
 
   photo.comments.push({comment: comment, user_id: userId});
   await photo.save();
-  console.log(entry)
+  console.log(entry);
 
-  response.status(200).json({comment: comment, user_id: userId});
-})
+  return response.status(200).json({comment: comment, user_id: userId});
+});
 
 const processFormBody = multer({storage: multer.memoryStorage()}).single("myImage");
 
 app.post('/photos/new', (request, response) => {
-  console.log("entering endpoint (Post, /photos/new)")
+  console.log("entering endpoint (Post, /photos/new)");
   processFormBody(request, response, function (err) {
     if (err || !request.file) {
-      response.status(400).send("Bad Request");
-      return
+      return response.status(400).send("Bad Request");
     }
 
     const timestamp = new Date().valueOf();
     const filename = 'U' +  String(timestamp) + request.file.originalname;
 
-    fs.writeFile("./images/" + filename, request.file.buffer, async function (err) {
+    fs.writeFile("./images/" + filename, request.file.buffer, async function (err1) {
       let photo = new Photo({file_name: filename, user_id: request.session.user.id, comments: []});
       await photo.save();
-      response.status(200).json(photo);
+      return response.status(200).json(photo);
   });
-
-  })
+    return response.status(500);
+  });
 });
 
 
